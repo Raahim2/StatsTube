ace7e75e6ac481e7f7f95171c415efd9ebf74a88
Raahim <127599591+Raahim2@users.noreply.github.com>
2024-04-07 12:53:02 +0530

    Update ThreeD-Geratator.py

--- pages/ThreeD-Geratator.py
+++ pages/ThreeD-Geratator.py
@@ -1,11 +1,11 @@
 import streamlit as st
-from utils import Turn_To_3D
+# from utils import Turn_To_3D
 
 st.title("3D GIF Generator")
 
-prompt = st.chat_input("Enter a prompt")
+# prompt = st.chat_input("Enter a prompt")
 
-if(prompt):
-    gif = Turn_To_3D(prompt)
+# if(prompt):
+#     gif = Turn_To_3D(prompt)
 
-    st.image(gif)
+#     st.image(gif)

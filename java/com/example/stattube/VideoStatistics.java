5b1f810bf3131a50b5d3fdde80e8d181d996f735
Raahim <127599591+Raahim2@users.noreply.github.com>
2024-08-01 19:52:10 +0530

    Update VideoStatistics.java

--- java/com/example/stattube/VideoStatistics.java
+++ java/com/example/stattube/VideoStatistics.java
@@ -59,7 +59,7 @@ public class VideoStatistics extends AppCompatActivity {
     private static final String APPLICATION_NAME = "My_First_Project";
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
     private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
-    private static final String API_KEY = "AIzaSyC8ocLxvWzGtpa_BS6Zp75FviHKkyOrFxI";
+    private static final String API_KEY = "API_KEY";
     String VIDEO_ID;
     String CHANNEL_SUBS;
     String CHANNEL_VIEW;
